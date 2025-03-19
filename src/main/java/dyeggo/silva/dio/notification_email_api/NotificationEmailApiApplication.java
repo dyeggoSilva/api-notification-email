d5ff60c037ea317f25ce3ac9f0fc58c4a8319aba
dyeggoSilva <dyeggo0102@live.com>
2025-03-18 23:17:04 -0300

    :construction: init: Criação do projeto
    Projeto base gerado pelo Spring initialzr e exportado para o repositório local para início de desenvolvimento.

---
+++ src/main/java/dyeggo/silva/dio/notification_email_api/NotificationEmailApiApplication.java
@@ -0,0 +1,13 @@
+package dyeggo.silva.dio.notification_email_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationEmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationEmailApiApplication.class, args);
+	}
+
+}
