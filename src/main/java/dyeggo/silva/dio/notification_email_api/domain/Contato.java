c0818003d63d65b80a883ab68fc48d141f6fe580
dyeggoSilva <dyeggo0102@live.com>
2025-03-19 02:50:38 -0300

    :construction:  criação das class de domínio da aplicação

---
+++ src/main/java/dyeggo/silva/dio/notification_email_api/domain/Contato.java
@@ -0,0 +1,29 @@
+package dyeggo.silva.dio.notification_email_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String menssagem;
+}
---
+++ src/main/java/dyeggo/silva/dio/notification_email_api/domain/Feedback.java
@@ -0,0 +1,30 @@
+package dyeggo.silva.dio.notification_email_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feedback {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String vagaConcorrida;
+    private String empresa;
+    private String feedback;
+}
