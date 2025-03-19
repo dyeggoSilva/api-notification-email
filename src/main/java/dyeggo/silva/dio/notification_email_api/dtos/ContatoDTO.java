c458a10113d555dcfbd50cfbe92901477d2cea93
dyeggoSilva <dyeggo0102@live.com>
2025-03-19 02:57:26 -0300

    :construction: Criação dos DTOs para Contato e Feedback

---
+++ src/main/java/dyeggo/silva/dio/notification_email_api/dtos/ContatoDTO.java
@@ -0,0 +1,4 @@
+package dyeggo.silva.dio.notification_email_api.dtos;
+
+public record ContatoDTO(String nome, String email, String telefone, String menssagem) {
+}
---
+++ src/main/java/dyeggo/silva/dio/notification_email_api/dtos/FeedbackDTO.java
@@ -0,0 +1,4 @@
+package dyeggo.silva.dio.notification_email_api.dtos;
+
+public record FeedbackDTO(String nome, String email, String telefone, String vagaConcorrida, String empresa, String feedback) {
+}
